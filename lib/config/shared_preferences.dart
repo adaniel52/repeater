fed9ab4926b9b470f4968a63861dcde275309810
Daniel <145444249+adaniel52@users.noreply.github.com>
2024-03-27 12:20:08 +0800

    Delete lib directory

--- lib/config/shared_preferences.dart
+++
@@ -1,7 +0,0 @@
-import 'package:shared_preferences/shared_preferences.dart';
-
-late SharedPreferences prefs;
-
-Future<void> initPreferences() async {
-  prefs = await SharedPreferences.getInstance();
-}
