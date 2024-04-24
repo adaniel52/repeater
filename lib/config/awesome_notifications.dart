61a62b24dc93a2076245cf6dd45089ff3e5a53ac
SM IMTIAZ YAYASAN TERENGGANU-KUALA BERANG | GURU 5 <tft5001.g5@m365a1.moe.edu.my>
2024-04-24 09:26:37 +0800

    remove logging

--- lib/config/awesome_notifications.dart
+++ lib/config/awesome_notifications.dart
@@ -1,18 +1,17 @@
-import 'dart:developer';
-
+// import 'dart:developer';
 import 'package:awesome_notifications/awesome_notifications.dart';
 
 class NotificationController {
   @pragma('vm:entry-point')
   static Future<void> onNotificationCreatedMethod(
       ReceivedNotification receivedNotification) async {
-    log('set, ${receivedNotification.id}');
+    // log('set, ${receivedNotification.id}');
   }
 
   @pragma('vm:entry-point')
   static Future<void> onNotificationDisplayedMethod(
       ReceivedNotification receivedNotification) async {
-    log('displayed, ${receivedNotification.id}');
+    // log('displayed, ${receivedNotification.id}');
   }
 
   @pragma('vm:entry-point')
