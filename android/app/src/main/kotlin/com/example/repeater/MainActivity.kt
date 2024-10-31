74386a7e81c6b00cd9f9d1ba6ce6c9911ad2ffb5
Daniel <m-13606438@moe-dl.edu.my>
2024-10-31 20:21:15 +0800

    blank screen on recent tray fix

--- android/app/src/main/kotlin/com/example/repeater/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/repeater/MainActivity.kt
@@ -3,3 +3,14 @@ package com.innovvice.repeater
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity()
+// blank screen on recent tray workaround - https://github.com/flutter/flutter/issues/66212#issuecomment-924980973
+{
+    protected override fun onPause() {
+        super.onPause()
+        try {
+            java.lang.Thread.sleep(200)
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+    }
+}
