7eb5d65f36d5ebffa6e9c2ab99c2c8bd934117c5
Daniel <m-13606438@moe-dl.edu.my>
2024-11-03 15:24:36 +0800

    very nice commit name..

--- lib/screens/home/schedule_details_screen.dart
+++ lib/screens/home/schedule_details_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:repeater/models/schedule_entry.dart';
+import 'package:repeater/widgets/custom_list_view.dart';
 
 class ScheduleDetailsScreen extends StatelessWidget {
   final ScheduleEntry scheduleEntry;
@@ -18,87 +19,56 @@ class ScheduleDetailsScreen extends StatelessWidget {
     final time = DateFormat.jm().format(scheduleEntry.startDate);
 
     return Scaffold(
-      body: Scrollbar(
-        child: CustomScrollView(
-          slivers: [
-            const SliverAppBar.medium(
-              title: Text('Schedule Details'),
-            ),
-            SliverListTile(
-              child: ListTile(
-                leading: const Icon(Icons.loop),
-                title: const Text('Review Type'),
-                trailing: Text(reviewType),
-              ),
-            ),
-            SliverListTile(
-              child: ListTile(
-                leading: const Icon(Icons.book),
-                title: const Text('Portion'),
-                trailing: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Text(juzNumber),
-                    Text(rubuNumbers),
-                  ],
-                ),
-              ),
-            ),
-            SliverListTile(
-              child: ListTile(
-                leading: const Icon(Icons.timer),
-                title: const Text('Time'),
-                trailing: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Text(date),
-                    Text(time),
-                  ],
-                ),
-              ),
-            ),
-            SliverListTile(
-              child: ListTile(
-                title: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    FilledButton(
-                      onPressed: () {},
-                      child: const Text('Mark as Completed'),
-                    ),
-                    OutlinedButton(
-                      onPressed: () {},
-                      child: const Text('Delete'),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
+      appBar: AppBar(
+        title: const Text('Schedule Details'),
       ),
-    );
-  }
-}
-
-class SliverListTile extends StatelessWidget {
-  const SliverListTile({
-    super.key,
-    required this.child,
-  });
-
-  final Widget child;
-
-  @override
-  Widget build(BuildContext context) {
-    return SliverList(
-      delegate: SliverChildBuilderDelegate(
-        (context, index) {
-          return child;
-        },
-        childCount: 1,
+      body: CustomListView(
+        children: [
+          ListTile(
+            leading: const Icon(Icons.loop),
+            title: const Text('Review Type'),
+            trailing: Text(reviewType),
+          ),
+          ListTile(
+            leading: const Icon(Icons.book),
+            title: const Text('Portion'),
+            trailing: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(juzNumber),
+                Text(rubuNumbers),
+              ],
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.timer),
+            title: const Text('Time'),
+            trailing: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(date),
+                Text(time),
+              ],
+            ),
+          ),
+          ListTile(
+            title: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                FilledButton(
+                  onPressed: () {},
+                  child: const Text('Mark as Completed'),
+                ),
+                OutlinedButton(
+                  onPressed: () {},
+                  child: const Text('Delete'),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
